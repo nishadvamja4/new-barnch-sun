ea5dcf3df80e7ad0c4855d5a2979dcde246242fa
nishadvamja4 <nishadvamjalive.com>
2024-04-23 17:48:11 +0530

    testfile with printformat

---
+++ ab/ab/doctype/gst_bill/gst_bill.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2024, ab and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class GSTBILL(Document):
+	pass
+
+
+
